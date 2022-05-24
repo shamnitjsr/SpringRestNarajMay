8cc79942ff2efe099dc8aa0a7a0b6d8b2d8811de
shamnitjsr <46891976+shamnitjsr@users.noreply.github.com>
2022-05-25 00:27:05 +0530

    commit_1

---
+++ SpringBootRestProj01-SimplePOC/src/main/java/com/nt/controller/MessageRenderController.java
@@ -0,0 +1,39 @@
+package com.nt.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // (@Controller + @ResponseBody on all the handler methods)
+@RequestMapping("/message") // Global Path (purely optional)
+public class MessageRenderController {
+	
+	@GetMapping("generate")
+	public ResponseEntity<String> generateMessage() {
+		
+		//get System date and time
+		LocalDateTime ldt = LocalDateTime.now();
+		//get current hour of the day(response body)
+		int hour = ldt.getHour();
+		String body = null;
+		if(hour<12)
+			body="Good Morning";
+		else if(hour<16)
+			body="Good Afternoon";
+		else if(hour<20)
+			body="Good Evening";
+		else
+			body="Good Night";
+		//response status
+		HttpStatus status=HttpStatus.OK;
+		//create ResponseEntity object
+		ResponseEntity<String> entity=new ResponseEntity<String>(body,status);
+		return entity;
+		
+	}
+
+}
